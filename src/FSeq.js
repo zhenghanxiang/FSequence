7d322cd2b0c907de3882f8a0ef5b6ed052991a80
ss <zpf19880@163.com>
2024-08-21 11:24:05 +0800

    1、调整基本列参数，消除有些展开不应出现后继时，出现后继的问题。2、调整fake逻辑[ω^2, ω^(ω+1)],[ω^2, ε0*ω],在这个版本不存在 [ω^ω^(ω+1),ω^ω]在这个版本的判定由不存在改为存在.

--- src/FSeq.js
+++ src/FSeq.js
@@ -25,8 +25,9 @@ export default class FSeq {
          *     If a != b, expand to [#,b](expand(a))
          */
         if ((this.isEmpty(a) && this.isEmpty(b)) || this.compareL(a, b) < 0) {
-            let result = this.getSupSeq1(subSeqs, subSeqs.length, a);
-            for (let i = 0; i < this.N; i++) {
+            const newA = this.expand(a);
+            let result = this.getSupSeq1(subSeqs, subSeqs.length-1, newA);
+            for (let i = 0; i < this.N-1; i++) {
                 result = this.getSupSeq1(subSeqs, subSeqs.length, result);
             }
             return result;
@@ -41,10 +42,6 @@ export default class FSeq {
     }
 
     static compareL(seq1, seq2) {
-        return this.compareL1(seq1, seq2, true, true);
-    }
-
-    static compareL1(seq1, seq2, extendcheck1, extendcheck2) {
         if (this.isEmpty(seq1) || this.isEmpty(seq2)) {
             return !this.isEmpty(seq1) ? -1 : !this.isEmpty(seq2) ? 1 : 0;
         }
@@ -70,15 +67,6 @@ export default class FSeq {
         const a2 = subSeqs2[subSeqs2.length - 1];
         const b2 = subSeqs2.length > 1 ? subSeqs2[subSeqs2.length - 2] : null;
 
-       /*  
-        *   如果level(a)<level(b) ;level(ψZ[#,b](a)) = level(ψZ(a))
-        *   如果level(a)>=level(b) ;
-        *       如果 level(a) >= level(ψZ(b)) 则 level(ψZ[#,b](a)) = level(a)
-        *       如果 b是后继
-        *               如果 level(ψZ(b)) > level(a)>=level(ψZ(b的前继))  则 level(ψZ[#,b](a)) = level(ψZ(b的前继))
-        *       否则  level(ψZ[#,b](a)) < level(ψZ(b))  且 level(ψZ[#,b](a)) 大于任何level(ψZ(b的前继))
-        */
-
         const compareL_a1_b1 = this.compareL(a1, b1);
         const compareL_a2_b2 = this.compareL(a2, b2);
         const compareL_b1_b2 = this.compareL(b1, b2);
@@ -86,122 +74,80 @@ export default class FSeq {
         if ((compareL_a1_b1 < 0 && compareL_a2_b2 < 0) || compareL_b1_b2 === 0) {
             return this.compareL2(a1, a2);
         } else if (compareL_a1_b1 < 0 && compareL_a2_b2 >= 0) {
-            if(this.isLimit(a1) && extendcheck1) {
+            if(this.isLimit(a1) ) {
                 /*
                  *  按a1为“真”，a2为“伪” 的情况时 走特殊规则； fffz 参数的真伪可以作为一个判定条件，来源@Arcahv 2024-8-16 11:59
                  *  compareL(ψZ(a1),ψZ[b2](a2)) = compareL(ψZ(a1),a2))
                  *  例如 ε0 = ψZ(ω)  ε(ω) = ψZ[ω^2](W^2)  compareL(ε0 ,ε(ω)) = compareL(ε0 ,ω^2) 
-                 * 
                  */
-                extendcheck1 = false;
                 let tempSeq2 = seq2;
                 while(true) {
                     const tempSubSeqs2 = this.getSubSeq(tempSeq2);
                     const tempA2 = tempSubSeqs2[tempSubSeqs2.length-1];
                     const tempA2SubSeqs = this.getSubSeq(tempA2);
                     if(tempA2SubSeqs.length==1) {
-                        return this.compareL1(seq1, tempA2, false, true);
+                        return this.compareL(seq1, tempA2);
                     }else {
                         tempSeq2 = tempA2;
                     }
                 }
             }
-            //如果level(a2)>=level(b2) ;
-            if (this.compareL(a2, this.getSupSeq0(b2)) >= 0) { 
-                //[ε0*(ω+1),ε0*ω*2],[ε0*(ω+1),ψ(Ω_ω*ω^ω)],[ε0*(ω^ω+1),ψ((Ω_ω)^ω)] 这几种情况会用到此规则
-                /*
-                 * 如果 level(a2) >= level(ψZ(b2)) 则 level(ψZ[#,b2](a2)) = level(a2)
-                 * 在这里compareL1(ψZ[#,b1](a1),ψZ[#,b2](a2)) = ompareL1(seq1, a2）
-                 */
-                return this.compareL1(seq1, a2, extendcheck1, true);
+        
+            if (!this.isLimit(b2) || this.compareL(a2, this.getSupSeq0(b2)) >= 0) { 
+                //这种情况 level(seq2) = level(a2） 决定
+                return this.compareL(seq1, a2);
             } else {
-                //如果 level(a2) < level(ψZ(b2))               
-                if(!this.isLimit(b2)) {
-                    //如果 b是后继
-					const b2_pre = this.expand(b2);
-					if(this.compareL(a2, this.getSupSeq0(b2_pre))>=0) {
-                        /*
-                         * 如果 level(ψZ(b)) > level(a)>=level(ψZ(b的前继))  则 level(ψZ[#,b](a)) = level(ψZ(b的前继))
-                         * 在这里level(ψZ[#,b1](a1)) = level(ψZ(a1)) 以及 level(ψZ[#,b2](a2)) = level(ψZ(b2_pre)) 
-                         * cmpareL1(ψZ[#,b1](a1), ψZ[#,b2](a2) = ompareL1(ψZ(a1), ψZ(b2_pre)) = ompareL1(a1, b2_pre);
-						 */
-                         return this.compareL2(a1, b2_pre);
-					} else {
-                        // 类似于 加法平移规则，来源@一只绵羊2333 2024-8-16 11:59 但乘法，指数情况还没考虑？，更高阶欲运算和高阶函数该怎么办？怎么推广到一般情况？
-                        // 其实我理解的fffz除了fake之外，只加入apply（作用）的概念，在这个基础上定义后继，加法等运算，类似于λ演算。
-						return this.compareL1(seq1, a2, extendcheck1, true);
-					}
-				}
                 /*
                  *如果 level(a2) < level(ψZ(b2)) , b不是后继
                  *否则  level(ψZ[#,b2](a2)) < level(ψZ(b2))  且 level(ψZ[#,b2](a2)) 大于level(ψZ[b2](b2))
                  */
-                if (this.compareL(a1, b2) === 0) {
-                    /*
-                     * 如果level(a1)==level(b2) 则 level(ψZ(a1）)==level(ψZ(b2)) 
-                     * 因level(a1)<level(b1),则level(ψZ[#,b1](a1)) = level(ψZ(a1))
-                     * compareL1(ψZ[#,b1](a1), ψZ[#,b2](a2))  = compareL1(ψZ(a1), ψZ[#,b2](a2))  = compareL1(ψZ(b2), ψZ[#,b2](a2))>0
-                     */
-                    return 1;
-                } else {
-                    /*
-                     *  如果level(a1)>level(b2)则level(ψZ[#,b1](a1))>level(ψZ(b2))>level(ψZ[#,b2](a2))
-                     *  如果level(a1)<level(b2)则level(ψZ[#,b1](a1))< level(ψZ[b2](b2))<ψZ[#,b2](a2))
-                     *  因此 compareL1(ψZ[#,b1](a1), ψZ[#,b2](a2)) = ompareL(a1, b2)
-                     */
-                    return this.compareL(a1, b2);
-                }
+                if(this.compareL2(a1, b2)>=0) {
+					return 1;
+				}else {
+					return -1;
+				}
             }
         } else if (compareL_a1_b1 >= 0 && compareL_a2_b2 < 0) {
             //为上面的镜像情况
-            if(this.isLimit(a2) && extendcheck2) {
-                extendcheck2 = false;
+            if(this.isLimit(a2)) {
                 let tempSeq1 = seq1;
                 while(true) {
                     const tempSubSeqs1 = this.getSubSeq(tempSeq1);
                     const tempA1 = tempSubSeqs1[tempSubSeqs1.length-1];
                     const tempA1SubSeqs = this.getSubSeq(tempA1);
                     if(tempA1SubSeqs.length==1) {
-                        return this.compareL1(tempA1, seq2, true, false);
+                        return this.compareL(tempA1, seq2);
                     }else {
                         tempSeq1 = tempA1;
                     }
                 }
             }
-            if (this.compareL(a1, this.getSupSeq0(b1)) >= 0) {
-                return this.compareL1(a1, seq2, true, extendcheck2);
+            if (!this.isLimit(b1) || this.compareL(a1, this.getSupSeq0(b1)) >= 0) {
+                return this.compareL(a1, seq2);
             } else {
-                if(!this.isLimit(b1)) {
-					const b1_pre = this.expand(b1);
-					if(this.compareL(a1, this.getSupSeq0(b1_pre))>=0) {
-						return this.compareL2(b1_pre, a2);
-					}else {
-						return this.compareL1(a1, seq2, true, extendcheck2);
-					}
+				if(this.compareL2(b1, a2)>0) {
+					return 1;
+				}else {
+					return -1;
 				}
-                if (this.compareL(b1, a2) === 0) {
-                    return -1;
-                } else {
-                    return this.compareL(b1, a2);
-                }
             }
         } else if (compareL_a1_b1 >= 0 && compareL_a2_b2 >= 0) {
             if (this.compareL(a1, this.getSupSeq0(b1)) >= 0 && this.compareL(a2, this.getSupSeq0(b2)) >= 0) {
                 /*
                  * 如果 level(a1) >= level(ψZ(b1)) 则 level(ψZ[#,b1](a1)) = level(a1)
                  * 如果 level(a2) >= level(ψZ(b2)) 则 level(ψZ[#,b2](a2)) = level(a2)
-                 * 在这里compareL1(ψZ[#,b1](a1),ψZ[#,b2](a2)) = ompareL1(a1, a2）
+                 * 在这里compareL(ψZ[#,b1](a1),ψZ[#,b2](a2)) = ompareL1(a1, a2）
                  */
                 return this.compareL(a1, a2);
             } else if (this.compareL(a1, this.getSupSeq0(b1)) >= 0) {
                 /*
                  * 如果 level(a1) >= level(ψZ(b1)) 则 level(ψZ[#,b1](a1)) = level(a1)
-                 * 在这里compareL1(ψZ[#,b1](a1),ψZ[#,b2](a2)) = ompareL1(a1, ψZ[#,b2](a2)）
+                 * 在这里compareL(ψZ[#,b1](a1),ψZ[#,b2](a2)) = ompareL1(a1, ψZ[#,b2](a2)）
                  */
-                return this.compareL1(a1, seq2, true, extendcheck2);
+                return this.compareL(a1, seq2);
             } else if (this.compareL(a2, this.getSupSeq0(b2)) >= 0) {
                 //为上面的镜像情况
-                return this.compareL1(seq1, a2, extendcheck1, true);
+                return this.compareL(seq1, a2);
             } else {
                 /*
                     *  如果b1是后继  那么 level(b2)>level(b1) ==> level(ψZ[b2](b2))>level(ψZ(b1)) 
@@ -219,9 +165,12 @@ export default class FSeq {
 
     static compareL2(seq1, seq2) {
 		if(!this.isLimit(seq1)&&!this.isLimit(seq2)) {
-			const successorPart1 = this.getSuccessorPartIntValue(seq1);
+			/*
+            const successorPart1 = this.getSuccessorPartIntValue(seq1);
 			const successorPart2 = this.getSuccessorPartIntValue(seq2);
 			return successorPart1 - successorPart2;
+            */
+            return this.compare(seq1,seq2);
 		}else {
 			return this.compareL(seq1, seq2);
 		}
